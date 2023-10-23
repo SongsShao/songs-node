7ed9fd145f577fc225af6b9cddf606b74dc7bbf1
songshao <ss01160899@alibaba-inc.com>
2023-10-23 10:57:12 +0800

    update version 0.0.27

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,77 +1,84 @@
-// const { execSync } = require('child_process');
-// const fs = require('fs');
-// let version;
-// let package;
+const { execSync } = require("child_process");
+const fs = require("fs");
+const { Signale } = require("signale");
+const path = require("path");
 
-// function updateVersion() {    
-//     fs.readFile('package.json', 'utf8', (err, data) => {
-//         if (err) {
-//             throw new Error(`读取文件时发生错误: ${err}`);
-//         }
-    
-//         package = JSON.parse(data);
-//         version = package.version;
-//         let versionArr = String(package.version).split('.');
-    
-//         versionArr[versionArr.length - 1] = Number(versionArr[versionArr.length - 1]) + 1;
-//         package.version = versionArr.join('.');
-//         fs.writeFile('package.json', JSON.stringify(package, '', 2), 'utf8', (err) => {
-//             if (err) {
-//                 throw new Error(`写入文件时发生错误: ${err}`);
-//             }
-//             console.log(`版本从${version}， 更新至${package.version}`);
-//             version = package.version;
-//             updateGiteePages();
-//         });
-//     });
-// };
+const logger = new Signale();
+let version;
+let package;
 
-// updateVersion();
+let packageJSONStr = fs
+  .readFileSync(path.join(__dirname, "../package.json"))
+  .toString("utf8");
 
-// console.log(version);
+function updateVersion() {
+  package = JSON.parse(packageJSONStr);
+  version = package.version;
+  let versionArr = String(package.version).split(".");
 
-// function updateGiteePages() {
-    
-//     const request = fetch('https://gitee.com/batype/songs-note/pages/rebuild', {
-//         method: 'POST', // 设置请求方法为POST
-//         headers: {
-//           'Content-Type': 'application/json', // 设置请求头信息
-//         },
-//         body: JSON.stringify(
-//             {
-//                 branch: 'gh-pages',
-//                 build_directory: '',
-//                 force_https: true,
-//                 auto_update: false
-//             }), // 将要发送的数据转换为JSON字符串
-//       });
+  versionArr[versionArr.length - 1] =
+    Number(versionArr[versionArr.length - 1]) + 1;
+  package.version = versionArr.join(".");
+  fs.writeFileSync(
+    path.join(__dirname, "../package.json"),
+    JSON.stringify(package, "", 2),
+    "utf8"
+  );
+  console.log(`版本从${version}， 更新至${package.version}`);
+  return package.version;
+}
 
-//       request.then(response => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return response.json(); // 解析响应数据为JSON对象
-//       }).then(res => {
-//         console.log('Gitee update Pages request is OK!');
-//       }).catch(err => {
-//         throw new Error(err);
-//       })
-// }
+updateVersion();
 
-// execSync(`git add . && git commit -m "update version ${version}" && git push origin master`, (err, data) => {
-//     if (err) {
-//         throw new Error(`git commit error: ${err}`);
-//     }
-//     console.log(`git add . && git commit -m "update version ${version}"`);
-// });
+function updateGiteePages() {
+  const request = fetch("https://gitee.com/batype/songs-note/pages/rebuild", {
+    method: "POST", // 设置请求方法为POST
+    headers: {
+      "Content-Type": "application/json", // 设置请求头信息
+    },
+    body: JSON.stringify({
+      branch: "gh-pages",
+      build_directory: "",
+      force_https: true,
+      auto_update: false,
+    }), // 将要发送的数据转换为JSON字符串
+  });
 
-// execSync('npm run release', (err, data) => {
-//     if (err) {
-//         throw new Error(`npm run release error: ${err}`);
-//     }
-//     console.log(`版本${version}发布成功！`);
-//     updateGiteePages();
-// });
+  request
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // 解析响应数据为JSON对象
+    })
+    .then((res) => {
+      console.log("Gitee update Pages request is OK!");
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+}
 
-
-
+(async () => {
+  let version = updateVersion();
+  let versionData;
+  try {
+    execSync(
+      `git add . && git commit -m "update version ${version}" && git push origin master`,
+      {
+        stdio: [0, 1, 2],
+      }
+    );
+    versionData = true;
+    console.log(`git add . && git commit -m "update version ${version}"`);
+  } catch (error) {
+    logger.error(e.message);
+    process.exit(0);
+  }
+  if (versionData) {
+    execSync("npm run release", {
+      stdio: [0, 1, 2],
+    });
+    updateGiteePages();
+  }
+})();
