f3a953a321144febce201bad10ea8a2a6318313f
ss01160899 <ss01160899@alibaba-inc.com>
2023-11-03 14:07:40 +0800

    update version 0.0.48

--- react/src/react-reconciler/src/ReactFiberCompleteWork.js
+++ react/src/react-reconciler/src/ReactFiberCompleteWork.js
@@ -1,19 +1,36 @@
-import { enableCache } from '../../shared/ReactFeatureFlags';
-import { Passive } from './ReactFiberFlags';
+import { enableCache } from "../../shared/ReactFeatureFlags";
+import { Passive } from "./ReactFiberFlags";
 
 export function completeWork(current, workInProgress) {
-    const newProps = workInProgress.pendingProps;
-    switch(workInProgress.tag) {
-        case HostRoot: {
-            updateHostContainer(current, workInProgress);
-            return null;
-        }
-        case HostComponent:
-            return updateHostComponent(current, workInProgress);
-
-        case HostText:
-            return null;
-        default:
-         return null;
+  const newProps = workInProgress.pendingProps;
+  switch (workInProgress.tag) {
+    case HostRoot: {
+      updateHostContainer(current, workInProgress);
+      return null;
     }
+    case HostComponent:
+      return updateHostComponent(current, workInProgress);
+
+    case HostText:
+      return null;
+    default:
+      return null;
+  }
+}
+
+function updateHostContainer(current, workInProgress) {
+  const portalOrRoot = workInProgress.stateNode;
+  // 检测fiber是否发生改变 true 未改变
+  const childrenUnchanged = hadNoMutationsEffects(current, workInProgress);
+  if (childrenUnchanged) {
+    // 不需要处理
+  } else {
+    const container = portalOrRoot.containerInfo;
+    appendAllChildrenToContainer(
+      newChildSet,
+      workInProgress,
+      /* needsVisibilityToggle */ false,
+      /* isHidden */ false
+    );
+  }
 }
