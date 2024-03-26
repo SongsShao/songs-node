ba2a056e31d54db95dd3eb611c72af87218da28d
ss01160899 <ss01160899@alibaba-inc.com>
2024-03-26 18:30:59 +0800

    update version 0.0.105

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,17 +1,19 @@
 import { createRoot } from "react-dom/client";
 
 let element = (
-  <div className="A1">
-    <div className="B1">
-        <div className="C1">C1</div>
-        <div className="C2">C2</div>
+  <div className='A1'>
+    <div className='B1'>
+      <div className='C1'>C1</div>
+      <div className='C2'>C2</div>
     </div>
-    <div className="B2">B2</div>
+    <div className='B2'>B2</div>
   </div>
 );
-const root = createRoot(document.getElementById("root"));
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
 console.log(element);
 console.log(root);
 
 root.render(element);
-
